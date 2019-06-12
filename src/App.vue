0a860ee6464c8ecabba53e14165c9215867f0361
Gustavo Henrique Cirulo <ghcirulo@indracompany.com>
2019-06-12 10:57:04 -0300

    Commit inicial para começar os estudos em Vue.JS

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <ul>
+      <li v-for="(foto, index) of fotos">
+        <h1>{{titulo + ' ' + (index+1)}}</h1>
+        <img :src="foto.url" :titulo="foto.titulo">
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  data() {
+    return {
+      titulo: "Dog",
+      fotos: [
+        {
+          url:
+            "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTwV4kVzT5McBdGSgqlVeRzubrNH_mOrrkKseDOGFURq20HmsrelEfMU7It",
+          titulo: "cachorro"
+        },
+        {
+          url:
+            "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTwV4kVzT5McBdGSgqlVeRzubrNH_mOrrkKseDOGFURq20HmsrelEfMU7It",
+          titulo: "cachorro"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="scss">
+</style>
